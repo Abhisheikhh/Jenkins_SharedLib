ddcd20cbcccd69bfac12bdb8d783b56917aa9916
Abhisheikhh <abhiac1236@gmail.com>
2026-01-13 11:25:36 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,5 +1,5 @@
 def call() {
-  withCredentials([usernamePassword(credentialsId: 'oss-index-creds', usernameVariable: 'OSS_INDEX_USERNAME', passwordVariable: 'OSS_INDEX_PASSWORD')]) {
+  withCredentials([usernamePassword(credentialsId: 'oss-index-credentials', usernameVariable: 'OSS_INDEX_USERNAME', passwordVariable: 'OSS_INDEX_PASSWORD')]) {
     def args = "--scan ./ --disableNodeAudit --ossIndexUsername $OSS_INDEX_USERNAME --ossIndexPassword $OSS_INDEX_PASSWORD"
     dependencyCheck additionalArguments: args, odcInstallation: 'OWASP'
   }
